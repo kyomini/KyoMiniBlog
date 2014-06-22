a4890a202d7b507dd65ab8093a2fad465ff2b66f
Kimis <xycc2007@yahoo.com.hk>
2014-06-22 11:29:35 +0800

    add

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+define('APP_PATH','./Blog/');
+define('DIR_SECURE_FILENAME', 'index.html');
+define('DIR_SECURE_CONTENT', 'No direct script access allowed!');
+require './Core/ThinkPHP.php';
