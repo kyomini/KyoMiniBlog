4e0b935c1b891107aaff8fb51da3bb4335412ee5
Kimis <xycc2007@yahoo.com.hk>
2014-07-04 15:21:26 +0800

    入口文件

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 define('APP_PATH','./Blog/');
+define("LANG_PATH", "./Blog/Lang/"); //定义，多语言目录
 define('DIR_SECURE_FILENAME', 'index.html');
 define('DIR_SECURE_CONTENT', 'No direct script access allowed!');
 require './Core/ThinkPHP.php';
