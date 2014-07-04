05f675b3789f1b15ee614a3ec8f5b8a8f100d30a
Kimis <xycc2007@yahoo.com.hk>
2014-07-04 15:22:03 +0800

    資料庫配置

--- config.inc.php
+++ config.inc.php
@@ -4,9 +4,9 @@ return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'tsst', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_NAME'   => '', // 数据库名
+    'DB_USER'   => '', // 用户名
+    'DB_PWD'    => '',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'Mi_', // 数据库表前缀
 
