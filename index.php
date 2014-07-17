8aeb2ef92535bae0a4b9716d76e9d316b4ba70dc
Kimis <xycc2007@yahoo.com.hk>
2014-07-17 15:48:57 +0800

    默認調式去掉調式模式

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (!file_exists('Blog/Runtime/Data/install.lock')) {
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+define('APP_DEBUG',false);
 define('APP_PATH','./Blog/');
 define("LANG_PATH", "./Blog/Lang/"); //定义，多语言目录
 define('DIR_SECURE_FILENAME', 'index.html');
