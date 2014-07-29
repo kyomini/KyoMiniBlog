812dc160b6877b2baa59eb0c56c714885655d8f2
KyoMini <kyomini@qq.com>
2014-07-29 09:48:36 +0800

    Update index.php
    更新

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ if (!file_exists('Blog/Runtime/Data/install.lock')) {
 	$_GET['m'] = 'Install';
 }
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+define('APP_DEBUG',false);
 define('APP_PATH','./Blog/');
 define("LANG_PATH", "./Blog/Lang/");
 define('DIR_SECURE_FILENAME', 'index.html');
